c3092c062adbe0c0e975d8f865263a07082c82c1
Mamunur Roshid Maheen <mamunurmaheen@gmail.com>
2021-08-06 11:17:36 +0600

    Initial commit

---
+++ class-3.py
@@ -0,0 +1,107 @@
+
+
+
+
+'''class 8 (function paramer pass)
+def sum(num1, num2, num3):
+    sum = num1 + num2 + num3
+    print(sum)
+
+sum(20, 60, 40)'''
+
+'''class 7 (simple function).....................
+def simpleFunction():
+    print("say hello to function")
+    a, b = 15, 30
+    sum  = a + b
+    print(sum)
+
+simpleFunction()
+simpleFunction()'''
+
+
+
+
+
+
+'''class 6 (while & for loop),,,,,,,,,,,,,,,,,,,,,,
+ for loop......................
+
+data = [5, 6, 7, 89, 77, 66, 77, 88, 99, 77, 00, text]
+
+for x in data:
+    print(x)'''
+
+''' while loop..................
+data = [5, 6, 7, 89, 77, 66, 77, 88, 99]
+i = 1
+while i<=8:
+    print(data[i])
+    i+=1'''
+
+
+''' class 5 while loop condition
+i = 1
+while i<=30:
+    if i%2 == 1:
+        print(i)
+
+    i+=1'''
+
+'''i = 1
+while i<=10:
+    print("I love myself", i)
+    i = i + 1
+    i+=1 concating loop'''
+#while condition:
+    #code
+    #condition
+
+''' class 4  use if + else + elif/ else if
+age = 20
+
+if(age >= 35):
+    print("Congratualations!!")
+    print("You are an adult Man.")
+    print(" Now,you can enjoys it.!!")
+
+else:
+    print("Sorry!")
+    print("You are not an adult")
+    print("you can't enjoys it, SORRY 'you are a Teenager!'")'''
+
+
+
+''' (class 3) concatination & variables math oprations
+x,y,e = (100, 3000,5555)
+
+print(x)
+print(y)'''
+
+'''text = " Fifty and Fifty makes "
+
+math = 25 + 25
+math1 = 25 + 25
+
+result = str(math + math1)
+
+print(text + result)'''
+
+'''myVar = "  I love you "
+
+name = "Hassan"
+
+fun = " mahamud "
+
+print(myVar + name + fun)'''
+
+
+''' class 2  (+),(-),(*),(/)
+english = 93
+bangla = 85
+math = 99
+economic = 25
+
+total = english+bangla+math+economic
+
+print("Your total subject mark is",total)'''
---
+++ test.py
@@ -0,0 +1,8 @@
+
+math = int(input(Enter Your Math Mark!))
+english = int(input(Enter Your English Mark!))
+bangla = int(input(Enter Your Bangla Mark!))
+
+total = math + english + bangla
+
+print(total)
